d20864a253d4451c7dcb18ae30fe7bb03b15f1ab
various.authors <various.authors@gmail.com>
2016-05-15 12:28:38 -0700

    initialize and draw a board

--- love/draw.lua
+++ love/draw.lua
@@ -1,5 +1,48 @@
 love.viewport = require('libs/viewport').newSingleton()
 
 function love.draw()
+    love.graphics.push()
+    love.graphics.scale(game.constants.scale, game.constants.scale)
     -- Draw here
+
+    board_draw(game.world.board)
+
+    love.graphics.pop()
+end
+
+function board_draw (board)
+
+    for y = 1, board.height, 1 do
+        for x = 1, board.width, 1 do
+            local cell = board[y][x]
+            local dim = game.constants.cell_dim
+            local offset = game.constants.offset
+            local y = (y - 1) * dim + (y * offset)
+            local x = (x - 1) * dim + (x * offset)
+
+            local r_width = dim * cell.ratios.r
+            local g_width = dim * cell.ratios.g
+            local b_width = dim * cell.ratios.b
+
+            love.graphics.setColor(RGB_COLORS[WHITE])
+            love.graphics.rectangle('line', x - 1, y - 1, dim + 2, dim + 2)
+
+            love.graphics.setColor(RGB_COLORS[RED])
+            local pos_x = x + 0
+
+            love.graphics.rectangle('fill', pos_x, y, r_width, dim)
+
+            love.graphics.setColor(RGB_COLORS[GREEN])
+            pos_x = pos_x + r_width
+
+            love.graphics.rectangle('fill', pos_x, y, g_width, dim)
+
+            love.graphics.setColor(RGB_COLORS[BLUE])
+            pos_x = pos_x + g_width
+
+            love.graphics.rectangle('fill', pos_x, y, b_width, dim)
+            pos_x = pos_x + b_width
+
+        end
+    end
 end
--- love/load.lua
+++ love/load.lua
@@ -1,4 +1,65 @@
+RED = "red"
+GREEN = "green"
+BLUE = "blue"
+WHITE = "white"
+
+RGB_COLORS = {
+    red = { 255, 0, 0 },
+    green = { 0, 255, 0 },
+    blue = { 0, 0, 255 },
+    white = { 255, 255, 255 },
+}
+
 function love.load()
     require('game/controls')
     require('game/sounds')
+
+    game.constants = { 
+        width = 5,
+        height = 5,
+        cell_dim = 5,
+        offset = 5,
+        scale = 5,
+    }
+    game.world = { }
+    game.state = { }
+
+    game.world.board = build_board(game.constants.width, game.constants.height)
+
+    game.player = {}
+    game.player.cursor = {
+        x = game.world.board.width / 2,
+        y = game.world.board.height / 2,
+    }
+end
+
+function build_board (width, height)
+    local board = {}
+
+    for y = 1, height, 1 do
+        board[y] = {}
+
+        for x = 1, width, 1 do
+            local cell = {
+                r = math.random(255),
+                g = math.random(255),
+                b = math.random(255),
+            }
+
+            local total = cell.r + cell.g + cell.b
+
+            cell.ratios = {
+                r = cell.r / total,
+                g = cell.g / total,
+                b = cell.b / total,
+            }
+
+            board[y][x] = cell
+        end
+    end
+
+    board.height = height
+    board.width = width
+
+    return board
 end
